0e49aa32f99399ef66c3f6a4b853f19cb43abf2d
Allan F <allanmatosf@hotmail.com>
2025-03-05 23:15:26 -0300

    adicionados metodos de agendamento de notificacoes

---
+++ src/main/java/com/example/agendamento_notificacao_api/business/AgendamentoService.java
@@ -0,0 +1,29 @@
+package com.example.agendamento_notificacao_api.business;
+
+
+import com.example.agendamento_notificacao_api.business.mapper.IAgendamentoMapper;
+import com.example.agendamento_notificacao_api.controller.dto.in.AgendamentoRecord;
+import com.example.agendamento_notificacao_api.controller.dto.out.AgendamentoRecordOut;
+import com.example.agendamento_notificacao_api.infrastructure.repositories.AgendamentoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+
+
+public class AgendamentoService {
+
+    private final AgendamentoRepository repository;
+    private final IAgendamentoMapper agendamentoMapper;
+
+    public AgendamentoRecordOut gravarAgendamento(AgendamentoRecord agendamento){
+         return agendamentoMapper.paraOut(
+                 repository.save(
+                         agendamentoMapper.paraEntity(agendamento)));
+
+    }
+
+
+
+}
---
+++ src/main/java/com/example/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
@@ -0,0 +1,19 @@
+package com.example.agendamento_notificacao_api.business.mapper;
+
+
+import com.example.agendamento_notificacao_api.controller.dto.in.AgendamentoRecord;
+import com.example.agendamento_notificacao_api.controller.dto.out.AgendamentoRecordOut;
+import com.example.agendamento_notificacao_api.infrastructure.entities.Agendamento;
+import org.mapstruct.Mapper;
+
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
+public interface IAgendamentoMapper {
+
+
+    Agendamento paraEntity(AgendamentoRecord agendamento);
+
+    AgendamentoRecordOut paraOut(Agendamento agendamento);
+}
